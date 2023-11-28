1d612c8102e742d469ff8a6d7fb616882211f11d
Yegor Savko <Yegor@MacBook-Pro-Yegor.local>
2023-11-29 00:51:11 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <active-user :username="user.name" :userage="user.age"></active-user>
+        <user-data @set-data="setUserData"></user-data>
+    </div>
+</template>
+
+<script>
+    export default{
+        data() {
+            return {
+                user:{
+                    name:'Name Surname',
+                    age:44,
+                }
+            }
+        },
+        methods: {
+            setUserData(name,age){
+                this.user={
+                    name:name,
+                    age: +age, // "+" convert to type number
+                }
+            }
+        },
+    }
+</script>
+
+<style >
+    html{
+        font-family: sans-serif;
+    }
+
+    section{
+        margin: 2rem auto;
+        max-width: 40rem;
+        border-radius:12px ;
+        border: 1px solid #ccc;
+        padding: 1rem;
+    }
+</style>
---
+++ src/components/ActiveUser.vue
@@ -0,0 +1,30 @@
+<template>
+    <section>
+        <h2>{{ username }}</h2>
+        <h3>{{ userage }} Years</h3>
+    </section>
+</template>
+
+<script>
+    export default{
+        props:{
+            username:{
+                type: String,
+                required:true,
+            },
+        userage:{
+            type: Number,
+            required:true,
+        }
+        }
+    };
+</script>
+
+<style >
+    section{
+        margin: 2rem auto;
+        max-width: 40rem;
+        border-radius:12px ;
+        border: 1px solid #ccc;
+    }
+</style>
